2f544c066209db3cec323280ac56eaef8c0f01dc
Grisha K <naspeh@gmail.com>
2009-11-17 13:00:21 +0200

    Updated sidebar; Implemened header; Updated main styles;

--- horosh/lib/views.py
+++ horosh/lib/views.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 from horosh.lib.base import current_user, render
+from horosh.model import meta
+from horosh import model
+from sqlalchemy import and_
+import logging
+
+log = logging.getLogger(__name__)
 
 def sidebar():
+    articles = meta.Session.query(model.Article).filter(
+        and_(model.Article.published != None, model.Article.label == None)
+    ).order_by(model.Article.created.desc()).all()
+
     params = dict(
-        user=current_user()
+        user=current_user(),
+        articles=articles
     )
     return render('/sidebar.html', params)
