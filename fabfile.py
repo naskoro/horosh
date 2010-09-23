ffb5d344d2e07eed33ca438760a0630980010cfb
Grisha K <naspeh@gmail.com>
2010-09-24 00:06:05 +0300

    Fixed fabfile, removed Makefile;

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from fabric.api import *
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def pep8(target='.'):
+    '''Run pep8'''
+    local('pep8 --ignore=E202 %s' % target, capture=False)
+
+
+def clean(mask=None):
+    '''Clean useless files'''
+    masks = [mask] if mask else ['*.pyc', '*.pyo', '*~', '*.orig']
+    command = ('find . -name "%s" -exec rm -f {} +' % mask for mask in masks)
+    local('\n'.join(command), capture=False)
+
+
+@hosts('root@yadro.org')
+def deploy():
+    '''Deploy to server'''
+    local('hg push', capture=False)
+    with cd('/var/www/horosh/'):
+        run('ls -la')
+        run('hg pull&&hg up')
+        run('/etc/init.d/horosh force-reload')
