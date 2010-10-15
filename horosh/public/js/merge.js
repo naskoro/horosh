ff18f7014f67dcb275b73749f7141d4a652116ab
Grisha K <naspeh@gmail.com>
2010-10-15 23:59:53 +0300

    Fixed merging of form;

--- horosh/public/js/merge.js
+++ horosh/public/js/merge.js
@@ -1,12 +1,11 @@
 (function($) {
     $.fn.merge = function(data) {
         $data = $(data);
-        $this = $(this);
-        if ($data.find('form').length) {
-            if(!$data.find('.error-message').length && !$this.find('.error-message').length) {
+        if ($data.find('form').length && this.find('form').length) {
+            if(!$data.find('.error-message').length && !this.find('.error-message').length) {
                 return;
             }
         }
-        $this.html(data);
+        this.html(data);
     };
 })(jQuery);
